550349ce0b900cbb757eab56f348f65080b21e0b
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-08-07 17:16:51 -0300

    add: logic for post data user

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit() {
+    this.authService.login(this.username, this.password)
+      .subscribe({
+        next: res => {
+          console.log('Login exitoso', res);
+          this.router.navigate(['/vista-librerias'])
+
+        },
+        error: err => {
+          console.log(err);          
+        }
+      });
+  }
 }
